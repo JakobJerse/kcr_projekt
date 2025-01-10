d9356612c2f53094b280b3bf30a06f4b9b167505
Nejc Skrbec <71454677+nejcskrbec@users.noreply.github.com>
2025-01-10 12:43:51 +0100

    feat: remote can be used for modal

--- static/js/script.js
+++ static/js/script.js
@@ -18,14 +18,6 @@ window.onload = function () {
 var isModalOpen = false;
 
 function openPreview(channelName, imageUrl, buttonText = 'Glej v živo', flag = false, genre = '', streamingPlatform = '', info = '', cast = '') {
-    
-    isModalOpen = true;
-    modalItems = [
-        document.getElementById('playButton'), 
-        document.getElementById('startOverButton'), 
-        document.getElementById('closeButton')];
-    focusIndex = focusables.indexOf(modalItems[0]);
-
 
     const modal = document.getElementById('previewModal');
     const title = document.getElementById('previewTitle');
@@ -36,6 +28,12 @@ function openPreview(channelName, imageUrl, buttonText = 'Glej v živo', flag = 
     const infoSpan = document.getElementById('previewInfo');
     const streamingPlatformSpan = document.getElementById('previewStreamingPlatform');
     const castSpan = document.getElementById('previewCast');
+
+    isModalOpen = true;
+    gridFocusIndex = focusIndex; // save focus index for when we close modal
+    modalItems = [playButton, startOverButton];
+    focusIndex = 0;
+    updateFocus();
 
     // Update modal content dynamically
     title.textContent = channelName;
@@ -49,6 +47,9 @@ function openPreview(channelName, imageUrl, buttonText = 'Glej v živo', flag = 
     playButton.innerHTML = `<i class="fa-solid fa-tv"></i> ${buttonText}`;
     if (flag) {
         startOverButton.innerHTML = `<i class="fa-solid fa-clock-rotate-left"></i> Glej od začetka`;
+        if (startOverButton.classList.contains('hidden')) {
+            startOverButton.classList.remove('hidden');
+        }
     } else {
         startOverButton.classList.add('hidden');
     }
@@ -65,6 +66,11 @@ function closePreview() {
     const modal = document.getElementById('previewModal');
     modal.style.display = 'none';
     isModalOpen = false;
+
+    focusables = Array.from(sidebarItems).concat([profileImage]).concat(allCards);
+    focusIndex = gridFocusIndex;
+
+    updateFocus();
 }
 
 function playChannel(channelName) {
@@ -74,13 +80,50 @@ function playChannel(channelName) {
 
 
 const socket = io();
+
+var focusIndex = 0;
+var gridFocusIndex = 0;
 var focusables = [];
+var sidebar = null;
 var sidebarItems = [];
 var modalItems = [];
+var profileImage = null;
+var allCards = [];
+var rows = []
+
+function updateFocus() {
+    focusables = !isModalOpen ? Array.from(sidebarItems).concat([profileImage]).concat(allCards) : modalItems;
+
+    focusables.forEach((item, index) => {
+        item.classList.toggle("focused", index === focusIndex);
+    });
+
+    if (focusables[focusIndex] === profileImage) {
+        profileImage.classList.add("focused");
+    } else {
+        profileImage.classList.remove("focused");
+    }
+
+    const focusedElement = focusables[focusIndex];
+    focusedElement.focus();
+
+    if (isModalOpen) {
+        focusedElement.classList.add("focused");
+    }
+
+    if (focusIndex < sidebarItems.length) {
+        const sidebarRect = sidebar.getBoundingClientRect();
+        const itemRect = focusedElement.getBoundingClientRect();
+        if (itemRect.top < sidebarRect.top) {
+            sidebar.scrollTop -= sidebarRect.top - itemRect.top;
+        } else if (itemRect.bottom > sidebarRect.bottom) {
+            sidebar.scrollTop += itemRect.bottom - sidebarRect.bottom;
+        }
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    let focusIndex = 0;
-    const sidebar = document.querySelector(".sidebar");
+    sidebar = document.querySelector(".sidebar");
     sidebarItems = [document.getElementById('home_button'),
         document.getElementById('search_button'),
         document.getElementById('movies_button'),
@@ -89,12 +132,12 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById('hbo_button'),
         document.getElementById('netflix_button'),
         document.getElementById('disney_button')]
-    const profileImage = document.getElementById('profileImage');
+    profileImage = document.getElementById('profileImage');
 
     const rowSelectors = [".row1", ".row2", ".row3", ".row4"];
     let rows = rowSelectors.map(selector => document.querySelector(selector));
     rows = rows.filter(row => row !== null);
-    const allCards = rows.flatMap(row => Array.from(row.querySelectorAll(".card")));
+    allCards = rows.flatMap(row => Array.from(row.querySelectorAll(".card")));
 
     focusables = Array.from(sidebarItems).concat([profileImage]).concat(allCards);
     const active = document.querySelector(".active");
@@ -102,38 +145,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     updateFocus();
 
-    function updateFocus() {
-        console.log(modalItems)
-        focusables = !isModalOpen ? Array.from(sidebarItems).concat([profileImage]).concat(allCards) : modalItems;
-
-        console.log(focusables)
-        console.log(focusIndex)
-        focusables.forEach((item, index) => {
-            item.classList.toggle("focused", index === focusIndex);
-        });
-
-        if (focusables[focusIndex] === profileImage) {
-            profileImage.classList.add("focused");
-        } else {
-            profileImage.classList.remove("focused");
-        }
-
-        const focusedElement = focusables[focusIndex];
-        focusedElement.focus();
-
-        if (focusIndex < sidebarItems.length) {
-            const sidebarRect = sidebar.getBoundingClientRect();
-            const itemRect = focusedElement.getBoundingClientRect();
-            if (itemRect.top < sidebarRect.top) {
-                sidebar.scrollTop -= sidebarRect.top - itemRect.top;
-            } else if (itemRect.bottom > sidebarRect.bottom) {
-                sidebar.scrollTop += itemRect.bottom - sidebarRect.bottom;
-            }
-        }
-    }
-
     function moveFocus(direction) {
-        if (focusIndex < sidebarItems.length + 1) {
+        if (!isModalOpen && focusIndex < sidebarItems.length + 1) {
             if (direction === "down") {
                 focusIndex = Math.min(focusIndex + 1, sidebarItems.length);
             } else if (direction === "up") {
@@ -144,41 +157,66 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             }
         } else {
-            const currentCard = focusables[focusIndex];
-            const currentRowIndex = rows.findIndex(row => row.contains(currentCard));
-            const currentRow = rows[currentRowIndex];
-            const cardsInRow = Array.from(currentRow.querySelectorAll(".card"));
-            const currentCardIndex = cardsInRow.indexOf(currentCard);
+            
+            var currentCard = null;
+            var currentRowIndex = 0;
+            var currentRow = null;
+            var cardsInRow = [];
+            var currentCardIndex = 0;
+
+            if (!isModalOpen) {
+                currentCard = focusables[focusIndex];
+                currentRowIndex = rows.findIndex(row => row.contains(currentCard));
+                currentRow = rows[currentRowIndex];
+                cardsInRow = Array.from(currentRow.querySelectorAll(".card"));
+                currentCardIndex = cardsInRow.indexOf(currentCard);
+            }
 
             if (direction === "left") {
-                if (currentCardIndex === 0) {
-                    focusIndex = 0;
-                } else {
-                    if (currentCardIndex > 0) {
-                        focusIndex = focusables.indexOf(cardsInRow[currentCardIndex - 1]);
-                    } else if (currentRowIndex > 0) {
-                        const prevRowCards = rows[currentRowIndex - 1].querySelectorAll(".card");
-                        if (prevRowCards.length) {
-                            focusIndex = focusables.indexOf(prevRowCards[prevRowCards.length - 1]);
+                if (!isModalOpen) {
+                    if (currentCardIndex === 0) {
+                        focusIndex = 0;
+                    } else {
+                        if (currentCardIndex > 0) {
+                            focusIndex = focusables.indexOf(cardsInRow[currentCardIndex - 1]);
+                        } else if (currentRowIndex > 0) {
+                            const prevRowCards = rows[currentRowIndex - 1].querySelectorAll(".card");
+                            if (prevRowCards.length) {
+                                focusIndex = focusables.indexOf(prevRowCards[prevRowCards.length - 1]);
+                            }
                         }
+                    }
+                } else {
+                    if (focusIndex == focusables.indexOf(document.getElementById('playButton'))) {
+                        focusIndex = focusables.indexOf(document.getElementById('startOverButton'));
                     }
                 }
             } else if (direction === "right") {
-                if (currentCardIndex < cardsInRow.length - 1) {
-                    focusIndex = focusables.indexOf(cardsInRow[currentCardIndex + 1]);
+                if (!isModalOpen) {
+                    if (currentCardIndex < cardsInRow.length - 1) {
+                        focusIndex = focusables.indexOf(cardsInRow[currentCardIndex + 1]);
+                    }
+                } else {
+                    if (focusIndex == focusables.indexOf(document.getElementById('startOverButton'))) {
+                        focusIndex = focusables.indexOf(document.getElementById('playButton'));
+                    }
                 }
             } else if (direction === "up") {
-                if (currentRowIndex > 0) {
-                    const prevRowCards = rows[currentRowIndex - 1].querySelectorAll(".card");
-                    if (prevRowCards.length) {
-                        focusIndex = focusables.indexOf(prevRowCards[0]);
+                if (!isModalOpen) {
+                    if (currentRowIndex > 0) {
+                        const prevRowCards = rows[currentRowIndex - 1].querySelectorAll(".card");
+                        if (prevRowCards.length) {
+                            focusIndex = focusables.indexOf(prevRowCards[0]);
+                        }
                     }
                 }
             } else if (direction === "down") {
-                if (currentRowIndex < rows.length - 1) {
-                    const nextRowCards = rows[currentRowIndex + 1].querySelectorAll(".card");
-                    if (nextRowCards.length) {
-                        focusIndex = focusables.indexOf(nextRowCards[0]);
+                if (!isModalOpen) {
+                    if (currentRowIndex < rows.length - 1) {
+                        const nextRowCards = rows[currentRowIndex + 1].querySelectorAll(".card");
+                        if (nextRowCards.length) {
+                            focusIndex = focusables.indexOf(nextRowCards[0]);
+                        }
                     }
                 }
             }
@@ -224,6 +262,8 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
         } else if (action === 'search') {
             window.location.href = "/search"
+        } else if (isModalOpen && action === 'back') {
+            closePreview();
         }
 
     });
